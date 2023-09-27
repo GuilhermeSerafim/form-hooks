b983a32e1b149b569db8fb2d07bcb75a14db5727
GuilhermeSerafim <guilerstudies@gmail.com>
2023-09-27 11:41:10 -0300

    Formatando projeto e criando componente DadosEntrega

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,15 +2,17 @@ import React, { useState } from "react";
 import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
+import DadosEntrega from "./DadosEntrega";
 
 //Esse formulario de cadastro que vai controlar os dados, logo jogamos os dados para outro componente
-function FormularioCadastro({aoEnviar, validarCPF}) {
-  return (
-    <>
-    <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
-    <DadosUsuario/>
-    </>
-  )
+function FormularioCadastro({ aoEnviar, validarCPF }) {
+	return (
+		<>
+			<DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
+			<DadosUsuario />
+			<DadosEntrega />
+		</>
+	);
 }
 
 export default FormularioCadastro;
